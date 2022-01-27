ed1febca1d2a61bfa4177387ee074d8ac5164c15
Crowdbotics <team@crowdbotics.com>
2022-01-27 08:26:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "my_fluke_33199",
+  url: "https://my_fluke_33199.botics.co",
+  api: "https://my_fluke_33199.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
